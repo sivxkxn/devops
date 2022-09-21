1dd8d7ebecd7cb624fc0c000b63b456b8d258c8a
sivxkxn <sivxkxn@gmail.com>
2022-09-21 21:46:57 +0300

    lab1

---
+++ lab1/syvokon/tests/yuliia-1-test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env roundup
+#
+#/ usage:  rerun stubbs:test -m syvokon -p yuliia [--answers <>]
+#
+
+# Helpers
+# -------
+[[ -f ./functions.sh ]] && . ./functions.sh
+
+# The Plan
+# --------
+describe "yuliia"
+
+# ------------------------------
+# Replace this test. 
+it_fails_without_a_real_test() {
+    exit 1
+}
+# ------------------------------
+
